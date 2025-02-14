a5dffaae11f9de1c2f59b2c3785eae39a700344b
galaxyapplication <galaxyapplicationerp@gmail.com>
2025-02-14 16:10:54 +0530

    sujal

--- src/dashboard/Profile.jsx
+++ src/dashboard/Profile.jsx
@@ -239,7 +239,7 @@ const Profile = () => {
                                              </div>
                                              <div className="detail-item">
                                                  <label><FaMobileScreenButton  className="icon-profile"/></label>
-                                                 <span>sujal</span>
+                                                 <span>sujal jethava</span>
                                              </div>
                                          </div>
                                 </div>
